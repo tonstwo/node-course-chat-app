3465927261e1976e35c7958ce1a9ccb0bd86c04a
Edward Anthony M. Polangcos <tonstwo@gmail.com>
2017-03-02 09:46:53 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Tonton',
-        text: 'Hello din!'
-    });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -13,15 +13,14 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
-
-    socket.emit('newMessage', {
-        from: 'Tonton',
-        text: 'Hallo There!',
-        createdAt: 123
-    });
     
     socket.on('createMessage', (newMessage) => {
         console.log('createMessage', newMessage);
+        io.emit('newMessage', {
+            from: newMessage.from,
+            text: newMessage.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
